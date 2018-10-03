2abae99eccf9c1af91bca828c56dbf216fa2a11f
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-03 15:22:41 +0200

    Web3 inject

--- src/index.js
+++ src/index.js
@@ -4,5 +4,30 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import cc from 'cryptocompare';
+const Web3 = require('web3');
+let web3js;
+
+
+cc.price('ETH', 'USD').then(prices => {
+  console.log("Eth price is: " + prices.USD);
+}).catch(e => {
+  console.error(e);
+});
+
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof window.web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3js = new Web3(window.web3.currentProvider);
+    console.log('Found web3');
+  } else {
+    console.log('No web3? You should consider trying MetaMask!');
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+})
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
