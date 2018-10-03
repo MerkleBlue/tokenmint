9f77625432f472868047f7cd959cfe9682b1fa40
BoleMerkle <jedibojan@gmail.com>
2018-10-03 15:27:40 +0200

    component placeholders

---
+++ src/app/components/MintTokens.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FormGroup } from '@material-ui/core';
+
+const MintTokens = () => {
+  return (
+    <FormGroup>
+      <p>This is a placeholder for MintTokens</p>
+    </FormGroup>
+  );
+};
+
+export default MintTokens;
---
+++ src/app/components/TokenOwner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FormGroup } from '@material-ui/core';
+
+const TokenOwner = () => {
+  return (
+    <FormGroup>
+      <p>This is a placeholder for TokenOwner</p>
+    </FormGroup>
+  );
+};
+
+export default TokenOwner;
