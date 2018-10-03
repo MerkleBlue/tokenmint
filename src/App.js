b2a4a9767f09aee479f0a5b849920ce30d641553
BoleMerkle <jedibojan@gmail.com>
2018-10-03 14:17:55 +0200

    deployment script

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Work in progress...</h1>
+          <h1 className="App-title">Work in progress!</h1>
         </header>
       </div>
     );
