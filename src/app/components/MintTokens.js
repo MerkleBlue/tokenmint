5792012e285b2fbfdfd8bc796082e99445fc52c3
BoleMerkle <jedibojan@gmail.com>
2018-10-04 18:45:36 +0200

    buttons styling

--- src/app/components/MintTokens.js
+++ src/app/components/MintTokens.js
@@ -4,7 +4,7 @@ import './css/MintTokens.css';
 const MintTokens = () => {
   return (
     <form className="main_form">
-      <a href="#" rel="noopener" className="btn btn-common wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms" >Mint tokens</a>
+      <span className="btn btn-common wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms" >Mint tokens</span>
     </form>
   );
 };
