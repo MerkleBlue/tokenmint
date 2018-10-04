728c33395efc75f9d9558e2cd9b6e2b72d36c016
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-04 14:43:14 +0200

    add openzeppelin and ganache cli

--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ import configureStore from './app/store/configureStore';
 
 import cc from 'cryptocompare';
 const Web3 = require('web3');
-let web3js;
+let web3;
 
 function getFee() {
     return new Promise((accept, reject) => {
         cc.price('ETH', 'USD').then(prices => {
-            console.log("Eth price is: " + prices.USD);
             console.log("Service fee is $100 which is: " + 1/prices.USD + "ETH");
             accept(1/prices.USD);
             return;
@@ -36,12 +35,20 @@ window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof window.web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
-    web3js = new Web3(window.web3.currentProvider);
-    console.log('Found web3 injected by the browser');
+    web3 = new Web3(window.web3.currentProvider);
+    console.log('Found web3 injected by the browser. Version: ' + web3.version);
   } else {
-    console.log('No web3? You should consider trying MetaMask!');
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+    console.log('Using http://localhost:7545 as web3 provider. Version: ' + web3.version); 
+
+    web3.eth.getAccounts().then(accounts => {
+        for(let account of accounts) {
+            web3.eth.getBalance(account).then(balance => {
+                console.log(account + ": " + balance);
+            });
+        }
+    });
   }
 })
 
