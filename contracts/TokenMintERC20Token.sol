ed477d6fe48f85223de3d81edbc9cd8d7f7d8ec5
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-04 18:45:30 +0200

    added standard erc20 token contract, enabled deployment

---
+++ contracts/TokenMintERC20Token.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.24;
+
+import '/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+
+/**
+ * @title Standard ERC20 token with optional functions implemented
+ *
+ * @dev Implementation of the basic standard token. For full specification see:
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ */
+contract TokenMintERC20Token is StandardToken {
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    constructor(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply) public {
+      name = _name;
+      symbol = _symbol;
+      decimals = _decimals;
+      totalSupply_ = _totalSupply;
+    }
+
+    // optional functions from ERC20 stardard
+
+    /**
+    * @dev Name of the ERC20 token
+    */
+    function name() view returns (string) {
+      return name;
+    }
+
+    /**
+    * @dev Symbol of the ERC20 token
+    */
+    function symbol() view returns (string) {
+      return symbol;
+    }
+
+    /**
+    * @dev Number of decimals of the ERC20 token
+    */
+    function decimals() view returns (uint8) {
+      return decimals;
+    }
+}
