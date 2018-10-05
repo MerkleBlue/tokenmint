309188ed2cb957d296f526e017bd1b51ac6fc956
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-05 17:05:46 +0200

    copy compiled contracts to src/contracts folder

---
+++ src/tools/copyContracts.js
@@ -0,0 +1,8 @@
+import fs from 'fs-extra';
+import chalk from 'chalk';
+
+fs.copy('build/contracts/TokenMintERC20Token.json', 'src/contracts/TokenMintERC20Token.json', err => {
+  if (err) return console.log(chalk.red(err)); // eslint-disable-line no-console
+  console.log(chalk.green("Contract TokenMintERC20Token ready!")); // eslint-disable-line no-console
+});
+
