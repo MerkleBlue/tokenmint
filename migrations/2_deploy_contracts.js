1193b50524dad15d0433ccc1343c1e1a85a9fec2
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-05 16:24:33 +0200

    instantiate new contract from json

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var TokenMintERC20Token = artifacts.require("./TokenMintERC20Token.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TokenMintERC20Token, "TMT", "Token Mint Token", 18, 1000000000);
+  deployer.deploy(TokenMintERC20Token, "Token Mint Token", "TMT", 18, 1000000000);
 };
--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import registerServiceWorker from './registerServiceWorker';
 import configureStore from './app/store/configureStore';
 
 import cc from 'cryptocompare';
+import ERC20TokenJSON from './contracts/TokenMintERC20Token.json'; 
+import TruffleContract from 'truffle-contract';
+
+let ERC20TokenContract, accounts;
+
+
 const Web3 = require('web3');
 let web3;
 
@@ -29,28 +35,61 @@ getFee().then(fee => {
 });
 
 
+function setupContracts() {
+    // instantiate it with truffle-contract
+    ERC20TokenContract = TruffleContract(ERC20TokenJSON);
+  
+    // Set the provider for our contracts
+    ERC20TokenContract.setProvider(web3.currentProvider);
 
+    // TODO: there's a bug with web3 1.0.
+    //dirty hack for web3@1.0.0 support for localhost testrpc, see https://github.com/trufflesuite/truffle-contract/issues/56#issuecomment-331084530
+    if (typeof ERC20TokenContract.currentProvider.sendAsync !== "function") {
+        ERC20TokenContract.currentProvider.sendAsync = function() {
+            return ERC20TokenContract.currentProvider.send.apply(
+                ERC20TokenContract.currentProvider, arguments
+            );
+        };
+    }
+}
+
+function instantiateERC20Contract(name, symbol, decimals, totalSupply, account) {
+    // try to instantiate new contract
+    ERC20TokenContract.new(name, symbol, decimals, totalSupply, {
+        from: account,
+        gas: 4712388,
+        gasPrice: 100000000000
+        }).then(function (instance) {
+        let contractInstance = instance;
+        //console.log(contractInstance);
+        contractInstance.name().then((name) => {
+            console.log(name);
+        }).catch((e) => {
+            console.error(e);
+        });
+    }).catch((e) => {
+        console.error(e);
+    });
+}
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof window.web3 !== 'undefined') {
-    // Use Mist/MetaMask's provider
-    web3 = new Web3(window.web3.currentProvider);
-    console.log('Found web3 injected by the browser. Version: ' + web3.version);
-  } else {
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-    console.log('Using http://localhost:7545 as web3 provider. Version: ' + web3.version); 
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        web3 = new Web3(window.web3.currentProvider);
+        console.log('Found web3 injected by the browser. Version: ' + web3.version);
+    } else {
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+        console.log('Using http://localhost:7545 as web3 provider. Version: ' + web3.version); 
 
-    web3.eth.getAccounts().then(accounts => {
-        for(let account of accounts) {
-            web3.eth.getBalance(account).then(balance => {
-                console.log(account + ": " + balance);
-            });
-        }
-    });
-  }
-})
+        web3.eth.getAccounts().then(allAccounts => {
+            accounts = allAccounts;
+            setupContracts();
+            instantiateERC20Contract("My new token", "MNT", 18, 1000, accounts[0]);
+        });
+    }
+});
 
 
 const store = configureStore();
