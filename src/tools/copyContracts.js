2ad38cba1ef5f36f3f29f3bf5dfba1db30f4c629
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-09 17:01:31 +0200

    added ERC223 contract

--- src/tools/copyContracts.js
+++ src/tools/copyContracts.js
@@ -6,3 +6,8 @@ fs.copy('build/contracts/TokenMintERC20Token.json', 'src/contracts/TokenMintERC2
   console.log(chalk.green("Contract TokenMintERC20Token ready!")); // eslint-disable-line no-console
 });
 
+fs.copy('build/contracts/TokenMintERC223Token.json', 'src/contracts/TokenMintERC223Token.json', err => {
+  if (err) return console.log(chalk.red(err)); // eslint-disable-line no-console
+  console.log(chalk.green("Contract TokenMintERC223Token ready!")); // eslint-disable-line no-console
+});
+
