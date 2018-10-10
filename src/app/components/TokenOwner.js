3c06a9587547481e464f7d28a584d78b1437d753
BoleMerkle <jedibojan@gmail.com>
2018-10-10 12:56:56 +0200

    connect accounts to TokenOwner component

--- src/app/components/TokenOwner.js
+++ src/app/components/TokenOwner.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import { TextField, Grid, Typography } from '@material-ui/core';
 import './css/TokenOwner.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const TokenOwner = () => {
-  return (
-    <form className="main_form">
-      <Grid container wrap="nowrap" spacing={8}>
-        <Grid item xs>
-          <TextField
-            required
-            id="outlined-required"
-            label="Token owner"
-            className="text_field"
-            margin="normal"
-            variant="outlined"
-          />
+class TokenOwner extends React.Component {
+  render() {
+    return (
+      <form className="main_form">
+        <Grid container wrap="nowrap" spacing={8}>
+          <Grid item xs>
+            <TextField
+              required
+              id="outlined-required"
+              label="Token owner"
+              className="text_field"
+              margin="normal"
+              variant="outlined"
+            />
+          </Grid>
+          <Grid item xs>
+            <Typography
+              align="left"
+              color="textSecondary"
+              variant="caption"
+              className="typography"
+            >
+              ETH address (not exchange address). This address will be owner of the token (after sale end date). <strong>Double check the address (and access to it) before submission!</strong>
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs>
-          <Typography
-            align="left"
-            color="textSecondary"
-            variant="caption"
-            className="typography"
-          >
-            ETH address (not exchange address). This address will be owner of the token (after sale end date). <strong>Double check the address (and access to it) before submission!</strong>
-          </Typography>
-        </Grid>
-      </Grid>
-    </form>
-  );
+      </form>
+    );
+  }
+}
+
+TokenOwner.propTypes = {
+  accounts: PropTypes.array.isRequired,
 };
 
-export default TokenOwner;
+function mapStateToProps(state) {
+  return {
+    accounts: state.accounts
+  };
+}
+
+export default connect(mapStateToProps)(TokenOwner);
