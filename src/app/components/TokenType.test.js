ee152f3d75158ea163077935c669bd40fe2c6026
BoleMerkle <jedibojan@gmail.com>
2018-10-18 11:59:00 +0200

    TokenType component unit tests finished

--- src/app/components/TokenType.test.js
+++ src/app/components/TokenType.test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import React from 'react';
 import { TokenType } from './TokenType';
-import { createMount } from '@material-ui/core/test-utils';
+import { createMount, createShallow } from '@material-ui/core/test-utils';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { JSDOM } from 'jsdom';
@@ -12,12 +12,20 @@ Enzyme.configure({ adapter: new Adapter() });
 describe("<TokenType /> tests", () => {
   let mount;
 
-  function setup(tokenType, tokenActions) {
+  function setup(tokenType) {
     const props = {
       tokenType: tokenType,
-      tokenActions: tokenActions
+      tokenTypeActions: { setTokenType: () => { } }
     };
     return mount(<TokenType {...props} />);
+  }
+
+  function setupShallow(tokenType, setTokenType) {
+    const props = {
+      tokenType: tokenType,
+      tokenTypeActions: { setTokenType: setTokenType }
+    };
+    return createShallow()(<TokenType {...props} />);
   }
 
   before(() => {
@@ -33,9 +41,9 @@ describe("<TokenType /> tests", () => {
   });
 
   it("renders TokenType with erc20 option", () => {
-    const wrapper = setup("erc20", {});
+    const wrapper = setup("erc20");
     expect(wrapper.props().tokenType).to.eq("erc20");
-    expect(wrapper.props().tokenActions).to.be.empty;
+    expect(wrapper.props().tokenTypeActions).to.not.be.empty;
     expect(wrapper.find("Grid").length).to.eq(3);
     expect(wrapper.find("RadioGroup").length).to.eq(1);
     expect(wrapper.find("RadioGroup").props().name).to.eq("tokenType");
@@ -49,13 +57,15 @@ describe("<TokenType /> tests", () => {
     expect(wrapper.find("FormControlLabel").at(1).props().labelPlacement).to.eq("start");
     expect(wrapper.find("Radio").length).to.eq(2);
     expect(wrapper.find("Radio").at(0).props().color).to.eq("default");
+    expect(wrapper.find("Radio").at(0).props().checked).to.be.true;
     expect(wrapper.find("Radio").at(1).props().color).to.eq("default");
+    expect(wrapper.find("Radio").at(1).props().checked).to.be.false;
   });
 
   it("renders TokenType with erc223 option", () => {
-    const wrapper = setup("erc223", {});
+    const wrapper = setup("erc223");
     expect(wrapper.props().tokenType).to.eq("erc223");
-    expect(wrapper.props().tokenActions).to.be.empty;
+    expect(wrapper.props().tokenTypeActions).to.not.be.empty;
     expect(wrapper.find("Grid").length).to.eq(3);
     expect(wrapper.find("RadioGroup").length).to.eq(1);
     expect(wrapper.find("RadioGroup").props().name).to.eq("tokenType");
@@ -69,13 +79,18 @@ describe("<TokenType /> tests", () => {
     expect(wrapper.find("FormControlLabel").at(1).props().labelPlacement).to.eq("start");
     expect(wrapper.find("Radio").length).to.eq(2);
     expect(wrapper.find("Radio").at(0).props().color).to.eq("default");
+    expect(wrapper.find("Radio").at(0).props().checked).to.be.false;
     expect(wrapper.find("Radio").at(1).props().color).to.eq("default");
+    expect(wrapper.find("Radio").at(1).props().checked).to.be.true;
   });
 
-  // it("simulates erc220 option click", () => {
-  //   const setTokenType = sinon.spy();
-  //   const wrapper = setup("erc20", { setTokenType: setTokenType });
-  //   wrapper.find("FormControlLabel").at(1).simulate("click");
-  //   expect(setTokenType.calledOnce).to.be.true;
-  // });
+  // for change event simulation we use shallow rendering because it doesn't work with createMount
+  it("simulates erc223 option click", () => {
+    const setTokenType = sinon.spy();
+    const wrapper = setupShallow("erc20", setTokenType);
+    wrapper.find("RadioGroup").simulate("change", {
+      target: { value: "erc223" }
+    });
+    expect(setTokenType.calledWith("erc223")).to.be.true;
+  });
 });
