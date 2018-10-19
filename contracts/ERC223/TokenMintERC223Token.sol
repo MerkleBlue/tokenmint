f5de7881ab80abd93c8d5d60ee5d6ea6853dc38d
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-10-19 11:19:27 +0200

    take fee for erc22 token

--- contracts/ERC223/TokenMintERC223Token.sol
+++ contracts/ERC223/TokenMintERC223Token.sol
@@ -11,12 +11,17 @@ import "./ERC223_Token.sol";
  */
 contract TokenMintERC223Token is ERC223Token {
 
-  constructor(string _name, string _symbol, uint8 _decimals, uint256 _initialAmount) public {
+  constructor(string _name, string _symbol, uint8 _decimals, uint256 _initialAmount, address feeReceiver) public payable {
     name = _name;
     symbol = _symbol;
     decimals = _decimals;
-    totalSupply = _initialAmount;
+    uint256 base = 10;
+    totalSupply = _initialAmount * base**decimals;
 
-    balances[msg.sender] = _initialAmount;
+    // set sender as owner of all tokens
+    balances[msg.sender] = totalSupply;
+
+    // pay for service fee for contract deployment
+    feeReceiver.transfer(msg.value);
   }
 }
