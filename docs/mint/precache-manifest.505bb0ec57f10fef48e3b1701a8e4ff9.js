fc9a6112b66ba4b2929b755e02e9bffdfac90478
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-11-13 17:02:13 +0100

    build 1.0.2

---
+++ docs/mint/precache-manifest.505bb0ec57f10fef48e3b1701a8e4ff9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79e48e02364d7a5fb950",
+    "url": "/mint/static/js/runtime~main.79e48e02.js"
+  },
+  {
+    "revision": "df8868788c32d195f2fa",
+    "url": "/mint/static/js/main.df886878.chunk.js"
+  },
+  {
+    "revision": "4b20a0877699813d6cd8",
+    "url": "/mint/static/js/1.4b20a087.chunk.js"
+  },
+  {
+    "revision": "df8868788c32d195f2fa",
+    "url": "/mint/static/css/main.d6d474f9.chunk.css"
+  },
+  {
+    "revision": "450694ee6a48884b610af51b629a586e",
+    "url": "/mint/index.html"
+  }
+];
--- docs/mint/service-worker.js
+++ docs/mint/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/mint/precache-manifest.db586eeab410f8bed9907b3dbddea5f1.js"
+  "/mint/precache-manifest.505bb0ec57f10fef48e3b1701a8e4ff9.js"
 );
 
 workbox.clientsClaim();
