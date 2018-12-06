e94c7cd0ea2cfaf30498b05933ec5ddb42afb5ff
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-12-06 16:04:44 +0100

    contracts tests: now tests for ERC tokens are used from openzeppelin (72 contract tests in total)

---
+++ contracts/test/TokenMintERC20Mock.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+import '../../node_modules/openzeppelin-solidity/contracts/mocks/ERC20Mock.sol';
+
+/**
+ * @title Used only to reference imported file so that ERC20Mock would be
+ * compiled and later used in contract tests
+ *
+ * @author TokenMint.io
+ *
+ */
+contract TokenMintERC20Mock is ERC20Mock {
+}
