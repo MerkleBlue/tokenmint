26f4d88d2a202596c06ef1a62fc991d5a5e9e331
KonstaMerkle <nebojsa.konsta@gmail.com>
2018-12-12 16:29:57 +0100

    update deploy script to deploy CrowdsaleToken

--- truffle-config.js
+++ truffle-config.js
@@ -6,12 +6,20 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
+      //gas: 0xfffffffffff,
+      //gasPrice: 0x01,
     },
     ropsten: {
       network_id: 3,
       host: '127.0.0.1',
       port: 8545,
       gas: 6000000
+    },
+    //solc: {
+    //  optimizer: {
+    //    enabled: true,
+    //    runs: 200
+    //  }
     }
   }
 };
