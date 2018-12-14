db19d06b0c20df1fcd236edf076aa64adab187f7
BoleMerkle <jedibojan@gmail.com>
2018-12-14 11:20:38 +0100

    InstallMetamask => InstallMetaMask

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import SuccessMessagePanel from './app/components/SuccessMessagePanel'; //eslint
 import { CSSTransitionGroup } from 'react-transition-group';
 import FullStory from 'react-fullstory';
 import pack from '../package.json';
-import InstallMetamaskPanel from './app/components/InstallMetaMaskPanel'; //eslint-disable-line import/no-named-as-default
+import InstallMetaMaskPanel from './app/components/InstallMetaMaskPanel'; //eslint-disable-line import/no-named-as-default
 import InstallCoinbasePanel from './app/components/InstallCoinbasePanel'; //eslint-disable-line import/no-named-as-default
 import HandlePaymentPanel from './app/components/HandlePaymentPanel'; //eslint-disable-line import/no-named-as-default
 
@@ -34,7 +34,7 @@ class App extends Component {
           transitionName="example"
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
-          <InstallMetamaskPanel />
+          <InstallMetaMaskPanel />
         </CSSTransitionGroup>
       ) : (
         <CSSTransitionGroup
--- src/app/components/InstallMetaMaskPanel.js
+++ src/app/components/InstallMetaMaskPanel.js
@@ -24,7 +24,7 @@ import * as mintApi from '../../api/mintApi';
 import MetaMaskDemoPlayer from './MetaMaskDemoPlayer';
 import DetectingWalletDialog from './DetectingWalletDialog'; //eslint-disable-line import/no-named-as-default
 
-export class InstallMetamaskPanel extends React.Component {
+export class InstallMetaMaskPanel extends React.Component {
 
   constructor(props) {
     super(props);
@@ -196,9 +196,8 @@ export class InstallMetamaskPanel extends React.Component {
   }
 }
 
-InstallMetamaskPanel.propTypes = {
+InstallMetaMaskPanel.propTypes = {
   isMobileDevice: PropTypes.bool.isRequired,
-  checkingNetwork: PropTypes.bool.isRequired,
   appStateActions: PropTypes.object.isRequired,
   accountsActions: PropTypes.object.isRequired,
   networkActions: PropTypes.object.isRequired,
@@ -220,4 +219,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(InstallMetamaskPanel);
+export default connect(mapStateToProps, mapDispatchToProps)(InstallMetaMaskPanel);
